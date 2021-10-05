d1d594776dd9751495aadbe7db8527193ee8a624
MicaioValente <micaiovalente@gmail.com>
2021-10-04 22:44:11 -0300

    feat(Home Comcluida) add styled-components

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import styled from 'styled-components';
-
+import {Content} from './styles'
 
 
 
@@ -9,17 +8,12 @@ import styled from 'styled-components';
 export default function Home() {
   return (
     <Content>
-      <h1>Olá</h1>
+      <h1>OLÁ</h1>
+    <div><a>Calendar</a></div>
+      
     </Content>
   );
 }
 
 
 
-const Content = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  height: 100vh;
-`
---
+++ src/pages/home/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(#3D2C8D, #916BBF);
+    h1{
+      color: #f5f5f5;
+      font-family: 'Oswald', sans-serif;
+    }
+    div{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #3D2C8D;
+      font-size: 17px;
+      color: #f5f5f5;
+      font-family: 'Oswald', sans-serif;
+      width: 100px;
+      height: 50px;
+      border-radius: 50px;
+      box-shadow: 0 0 20px #916bbf;
+      cursor: pointer;
+    }
+`
